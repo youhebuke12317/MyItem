cda9f69255e4c41a67b9c0b999d35683220a8f02
youhebuke12317 <zldata@aliyun.com>
2018-04-09 13:14:03 +0800

    剑指offer练习

---
+++ Offer/03_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix_01.cpp
@@ -0,0 +1,20 @@
+/**
+* @file			FindInPartiallySortedMatrix_01.cpp
+* @brief		Sample project head file
+* @detils		The header file details
+* @auther		Auther
+* @version		1.0.0.1
+* @date			2018年04月09日 星期一 13时11分46秒
+*/
+
+#include <iostream>
+using namespace std;
+
+bool Find(int *matrix, int rows, int columns, intnumber)
+{
+	bool found = false;
+
+	if (matrix != NULL && rows > 0 && columns > 0) {
+		
+	}
+}
